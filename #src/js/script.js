bb8d73db1d1b5b288873be09666ee902a78bfd12
Anton <dantrag.b@gmail.com>
2023-07-14 00:24:19 +0300

    tired from product. but comlete

--- #src/js/script.js
+++ #src/js/script.js
@@ -24,29 +24,51 @@ function smoothScroll() {
   }
 };
 smoothScroll();
+////////////////////////////// start for rpoductpage //////////////////////////////
 
-const body = document.querySelector('body');
-const blure_filter = document.querySelector('.blure__filter-bg');
-const filters_container = document.querySelector('.products-store__filters-container');
-const filters_arrow = document.querySelector('.filters-header__arrow');
-const sidebar_toggle_btn = document.querySelector('.filter__sidebar-toggle-btn');
-const products_store_main_containern = document.querySelector('.products-store__main-container');
-// console.log('blure_filter', blure_filter);
-// console.log('filters_arrow', filters_arrow);
-// console.log('filters_container', filters_container);
-// console.log('sidebar_toggle_btn', sidebar_toggle_btn);
-// console.log('products_store_main_containern', products_store_main_containern);
+// const body = document.querySelector('body');
+// const blure_filter = document.querySelector('.blure__filter-bg');
+// const filters_container = document.querySelector('.products-store__filters-container');
+// const filters_arrow = document.querySelector('.filters-header__arrow');
+// const sidebar_toggle_btn = document.querySelector('.filter__sidebar-toggle-btn');
+// const products_store_main_containern = document.querySelector('.products-store__main-container');
 
-function toggleSideBar() {
-  body.classList.toggle('lock');
-  blure_filter.classList.toggle('active');
-  filters_container.classList.toggle('active');
-}
+// function toggleSideBar() {
+//   body.classList.toggle('lock');
+//   blure_filter.classList.toggle('active');
+//   filters_container.classList.toggle('active');
+// }
 
-sidebar_toggle_btn.addEventListener('click', () => {
-  toggleSideBar();
+// sidebar_toggle_btn.addEventListener('click', () => {
+//   toggleSideBar();
+// });
+
+// filters_arrow.addEventListener('click', () => {
+//   toggleSideBar();
+// });
+
+////////////////////////////// end for rpoductpage //////////////////////////////
+
+const seiperr = document.querySelector('.product__inner__swiper-container');
+console.log(seiperr);
+
+
+let product_swiper2 = new Swiper('.product__inner__swiper-container2', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 2,
+  freeMode: true,
+  watchSlidesProgress: true,
 });
 
-filters_arrow.addEventListener('click', () => {
-  toggleSideBar();
+let product_swiper = new Swiper('.product__inner__swiper-container', {
+  loop: true,
+  navigation: {
+    nextEl: ".product__swiper-button-next",
+    prevEl: ".product__swiper-button-prev",
+  },
+  thumbs: {
+    swiper: product_swiper2,
+  },
 });
+
